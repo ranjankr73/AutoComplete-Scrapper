9f58a513dd7b6aea3b19f983bfbb310693e02799
Ranjan Kumar <ranjan73.me@gmail.com>
2025-03-21 18:44:14 +0530

    make it modular and query sent for 2 character

--- scrapper.js
+++ scrapper.js
@@ -1,27 +1,19 @@
-const axios = require('axios');
 const fs = require('fs');
+const { fetchNames, discoveredNames } = require('./fetchNames');
 
-const API_URL = 'http://35.200.185.69:8000/v1/autocomplete?query=';
 const CHARACTERS = 'abcdefghijklmnopqrstuvwxyz';
-const discoveredNames = new Set();
-
-async function fetchNames(query) {
-    try {
-        const response = await axios.get(API_URL + query);
-        if (response.data && Array.isArray(response.data.results)) {
-            response.data.results.forEach(name => discoveredNames.add(name));
-        }
-    } catch (error) {
-        console.error(`Error fetching '${query}':`, error.response ? error.response.data : error.message);
-    }
-}
 
 async function exploreAPI() {
-    for (const char of CHARACTERS) {
-        console.log(`Fetching names for '${char}'`);
-        await fetchNames(char);
-    }
+    const tasks = [];
     
+    for (const char1 of CHARACTERS) {
+        for (const char2 of CHARACTERS) {
+            tasks.push(fetchNames(char1 + char2));
+        }
+    }
+
+    await Promise.allSettled(tasks);
+
     console.log(`Total names discovered: ${discoveredNames.size}`);
     fs.writeFileSync('names.json', JSON.stringify([...discoveredNames], null, 2));
     console.log('Names saved to names.json');
